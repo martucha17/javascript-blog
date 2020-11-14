41a2d34d9c2a35f7b68ef072145276c16900dc8f
Marta Bruczko <marta.bruczko@gmail.com>
2020-11-14 16:53:56 +0100

    Add class action

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 'use strict';
 
 const titleClickHandler = function (event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
+    
 
-    /* remove class 'active' from all article links  */
+    /* [done] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [in progress] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    
+    this.classList.add('active')
 
-    /* remove class 'active' from all articles */
+    /*  [done] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles) {
